815da25552b1e574a80cdc0c1619cb0be429af11
François Caremoli <francois.caremoli@wanadoo.fr>
2024-08-23 16:08:54 +0200

    Ajout méthode POST

--- src/main/java/com/bigcorp/stock/correction/rest/controller/ManagerRestController.java
+++ src/main/java/com/bigcorp/stock/correction/rest/controller/ManagerRestController.java
@@ -1,19 +1,25 @@
 package com.bigcorp.stock.correction.rest.controller;
 
 import com.bigcorp.stock.correction.model.Manager;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/managers")
 public class ManagerRestController {
 
-    @GetMapping("managers/{id}")
+    @GetMapping("/{id}")
     public Manager getManager(@PathVariable("id") Long id){
         Manager manager = new Manager();
         manager.setId(id);
-        manager.setNom("Jacques");
+        manager.setNom("JsdsdanJean");
         manager.setSalaire(3000);
+        return manager;
+    }
+
+    @PostMapping
+    public Manager save(@RequestBody Manager manager){
+        manager.setNom("J'ai sauvegardé ce manager");
         return manager;
     }
 
